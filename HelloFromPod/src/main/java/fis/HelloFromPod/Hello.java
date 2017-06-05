83a91a338f9671849562efb7ab2a88620259c0a3
pprosser <pprosser@192.168.1.101>
2017-06-05 17:20:15 +0100

    First commit

---
+++ HelloFromPod/src/main/java/fis/HelloFromPod/Hello.java
@@ -0,0 +1,28 @@
+package fis.HelloFromPod;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class Hello {
+	String message;
+
+	public String getMessage() {
+
+		try {
+			message = "Hello from POD: " + InetAddress.getLocalHost().getHostName();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			message = "Hello, no hostname found";
+
+		}
+		return message;
+	}
+
+	@Override
+	public String toString() {
+
+		return getMessage();
+	}
+
+}
