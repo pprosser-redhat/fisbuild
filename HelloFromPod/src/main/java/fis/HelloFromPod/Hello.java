873af2f82d58f342ca0240c20d37c96197798297
pprosser <pprosser@192.168.1.103>
2018-02-16 17:18:41 +0000

    Change made

--- HelloFromPod/src/main/java/fis/HelloFromPod/Hello.java
+++ HelloFromPod/src/main/java/fis/HelloFromPod/Hello.java
@@ -9,7 +9,7 @@ public class Hello {
 	public String getMessage() {
 
 		try {
-			message = "Hello from POD:- " + InetAddress.getLocalHost().getHostName();
+			message = "Hello from POD with a little change:- " + InetAddress.getLocalHost().getHostName();
 		} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
